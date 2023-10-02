2c25a5c621dfc4eb577b7013edaab8bd6664b358
Rayshawn <rayshawntan124@gmail.com>
2023-10-02 17:06:43 +0800

    Fix Code Styling

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor for Remark.
+     *
+     * @param remark represents the given remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
@@ -20,7 +25,7 @@ public class Remark {
     }
 
     @Override
-    public boolean equals (Object other) {
+    public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof Remark //instanceof handles nulls
                 && value.equals(((Remark) other).value)); // state check
